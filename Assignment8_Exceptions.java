a9e51bb295a0f837c0420e70a93a5778fdd9fe66
AnkitZemoso <90907819+AnkitZemoso@users.noreply.github.com>
2021-09-28 14:04:27 +0530

    Update Assignment8_Exceptions.java

--- Assignment8_Exceptions.java
+++ Assignment8_Exceptions.java
@@ -1,7 +1,11 @@
 package com.ankit.JavaAssignment;
 
 import java.util.Scanner;
+/*
+Create three new types of exceptions. Write a class with a method that throws all three. In main( ), call the method but only use a single catch clause 
+that will catch all three types of exceptions. Add a finally clause and verify that your finally clause is executed, even if a NullPointerException is thrown.
 
+*/
 class InvalidMarksException extends Exception {
     public InvalidMarksException(String exception){
         super(exception);
