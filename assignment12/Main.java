6e9f00f903c9b09d2ad145285383a2f8100919f7
AnkitZemoso <90907819+AnkitZemoso@users.noreply.github.com>
2021-09-28 14:08:46 +0530

    Update Main.java

--- assignment12/Main.java
+++ assignment12/Main.java
@@ -2,7 +2,24 @@ package ankit.assignment12;
 
 import java.util.ArrayList;
 import java.util.List;
+/*
+Assignment on Stream Api
 
+Make a Student class having fields id,name,age,gender,engDepartment,year of enrollment ,perTillDate.
+Use stream api on above list of employees find
+
+1. Print the name of all departments in the college?
+2.Get the names of all students who have enrolled after 2018?
+3. Get the details of all male student in the computer sci department?
+4. How many male and female student are there ? (HINT:use Collectors.groupingBy() for grouping based on gender)
+5.What is the average age of male and female students?
+6.Get the details of highest student having highest percentage ?
+7.Count the number of students in each department? (Hint :use Collectors.groupingBy())
+8. What is the average percentage achieved in each department?
+9. Get the details of youngest male student in the Electronic department?(Hint :Use Collectors.minBy)
+10.How many male and female students are there in the computer science department?
+
+*/
 public class Main {
     public static void main(String[] args) {
 
