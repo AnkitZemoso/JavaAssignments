086b75fc1e59fa53b60aa7774454bd6a1e00522a
AnkitZemoso <90907819+AnkitZemoso@users.noreply.github.com>
2021-09-28 14:02:15 +0530

    Update Main.java

--- assignment7/Ques3/Main.java
+++ assignment7/Ques3/Main.java
@@ -1,5 +1,9 @@
 package ankit.assignment7.Ques3;
-
+/*
+Create three interfaces, each with two methods. Inherit a new interface that combines the three, adding a new method. 
+Create a class by implementing the new interface and also inheriting from a concrete class. 
+Now write four methods, each of which takes one of the four interfaces as an argument. In main( ), create an object of your class and pass it to each of the methods.
+*/
 public class Main {
     void a(Interface1 i){
         i.method1();
