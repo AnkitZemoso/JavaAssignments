f834959eb728a1e60a47cc176e104d85a990c2b9
AnkitZemoso <90907819+AnkitZemoso@users.noreply.github.com>
2021-09-28 14:01:16 +0530

    Update Main.java

--- assignment7/Ques2/Main.java
+++ assignment7/Ques2/Main.java
@@ -2,7 +2,11 @@ package ankit.assignment7.Ques2;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/*
+Create a Cycle class, with subclasses Unicycle, Bicycle and Tricycle. Add a balance( ) method to Unicycle and Bicycle, but not to Tricycle. 
+Create instances of all three types and upcast them to an array of Cycle. 
+Try to call balance( ) on each element of the array and observe the results. Downcast and call balance( ) and observe what happens.
+*/
 public class Main {
     public static void main(String[] args) {
         List<Cycle> cycles=new ArrayList<Cycle>();
