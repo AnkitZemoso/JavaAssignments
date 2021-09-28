673ae60b8f02a81d52c463cecfd77c04acf1d50c
AnkitZemoso <90907819+AnkitZemoso@users.noreply.github.com>
2021-09-28 13:53:20 +0530

    Update Main.java

--- assignment5/main/Main.java
+++ assignment5/main/Main.java
@@ -2,7 +2,11 @@ package ankit.assignment5.main;
 
 import ankit.assignment5.data.DefaultClass;
 import ankit.assignment5.singleton.SingletonClass;
+/*
+Create main class in package yourname.assignment.main, create object of first class and call both the methods to print the values. 
+Create object of second class using static method and then call the other method to print the String.
 
+*/
 public class Main {
     public static void main(String[] args) {
 
