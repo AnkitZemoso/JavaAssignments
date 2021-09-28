702ab9fa2563f428f134d1542eb608a63e9ab7a7
AnkitZemoso <90907819+AnkitZemoso@users.noreply.github.com>
2021-09-28 14:03:44 +0530

    Update DemoClass.java

--- assignment7/Ques5/DemoClass.java
+++ assignment7/Ques5/DemoClass.java
@@ -1,5 +1,9 @@
 package ankit.assignment7.Ques5;
+/*
+Create a class with an inner class that has a non-default constructor (one that takes arguments). 
+Create a second class with an inner class that inherits from the first inner class.
 
+*/
 public class DemoClass {
     static class InnerClass{
         protected String text;
